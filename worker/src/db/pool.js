8743ef1f63d12871e3e8d8e11fce1d84edfc74bd
takoyaki <coffemate1123@gmail.com>
2026-05-23 16:10:56 +0800

    Fix frontend API base/proxy for Worker in dev and prod

--- worker/src/db/pool.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const { dbUrl } = require('../config/env');
-
-const pool = new Pool({
-  connectionString: dbUrl,
-  ssl: { rejectUnauthorized: false },
-});
-
-module.exports = pool;
